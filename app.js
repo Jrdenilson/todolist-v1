ea18780480534f7ed14f1a42d732e3e57f3c2012
Denilson Junior <jrdenilson1@outlook.com>
2023-02-09 15:02:28 -0300

    Putting mongoose on the project

--- app.js
+++ app.js
@@ -1,46 +1,145 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
-const newTasks = ["Buy Food", "Cook Food", "Eat Food" ];
-const workTasks = [];
+//const newTasks = ["Buy Food", "Cook Food", "Eat Food" ];
+//const workTasks = [];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost:27017/todolistDB');
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+})
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const todo = new Item({
+    name: "Buy Food"
+});
+
+const todo1 = new Item({
+    name: "Make Food"
+});
+
+const todo2 = new Item({
+    name: "Eat Food"
+});
+
+const defaultItem = [todo, todo1, todo2];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function (req, res) {
-    const currentDay = date.getDate();   
 
-    res.render("list", {kindOfDay: currentDay, addTask: newTasks});
+    
+
+    Item.find(function (err, newTasks) {
+
+        if (newTasks.length === 0) {
+                    Item.insertMany(defaultItem, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully added default items");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", {kindOfDay: "Today", addTask: newTasks});
+        }   
+    });
+    
+    
+});
+
+app.get("/:listName", function (req, res) {
+    let nameRoute = _.capitalize(req.params.listName);
+
+    List.findOne({name: nameRoute}, function (err, result) {
+        if (!err) {
+            if (!result) {
+                const list = new List({
+                name: nameRoute,
+                items: defaultItem
+                });
+
+                list.save();  
+                res.redirect("/" + nameRoute);
+            } else {
+                res.render("list", {kindOfDay: result.name, addTask: result.items});
+            }
+        }
+    })
+    
+    
+
+    
+
+    
+
+});
+
+app.post("/", function (req, res) {
+    const item = req.body.newToDo;
+    const routeName = req.body.list;
+
+    console.log(routeName);
+    const newItem = new Item({
+        name: item
+    });
+
+    if (routeName === "Today") {
+        newItem.save();
+        res.redirect("/");
+    } else {
+        List.findOne({name: routeName}, function (err, foundList) {
+            foundList.items.push(newItem);
+            foundList.save();
+            res.redirect("/" + routeName);
+        })
+    }
+
+    
+});
+
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
 
 
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (!err) {
+            console.log("Successfully deleted item!");
+            res.redirect("/");
+        }
+           });
+    } else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function (err, foundList) {
+            if (!err) {
+                res.redirect("/" + listName);
+            }
+        } )
+    }
+
+    
     
 });
 
 
 
-app.post("/", function (req, res) {
-    const item = req.body.newToDo;
-
-     
-    if (req.body.list === "Work") {
-        workTasks.push(item);
-        res.redirect("/work")
-    } else {
-        newTasks.push(item)
-        res.redirect("/")
-    }
-    
-})
-
-
-app.get("/work", function (res, res) {
-    
-    res.render("list", {kindOfDay: "Work List", addTask: workTasks});
-
-})
 
 
 app.get("/about", function (req, res) {
