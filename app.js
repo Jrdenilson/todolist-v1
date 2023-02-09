ef7147c4e9db72a4afad29af7077c4505b72f45e
Denilson Junior <jrdenilson1@outlook.com>
2023-02-09 17:32:28 -0300

    testing ports'

--- app.js
+++ app.js
@@ -155,7 +155,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
 app.listen(port, function () {
     console.log("Server is running on port 3000!");
